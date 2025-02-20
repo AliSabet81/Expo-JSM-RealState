1f92e1d891537c58cc19397d4f5d9f6c5ce2091b
AliSabet81 <sabet.farshid1381@gmail.com>
2025-02-20 09:24:51 +0330

    routes

---
+++ app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,31 @@
+import { Tabs } from "expo-router";
+
+const TabsLayout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
