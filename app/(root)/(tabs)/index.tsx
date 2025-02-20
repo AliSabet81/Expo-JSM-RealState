37a92f8a3ec619eef4886e1a10e646a836232567
ali sabet <sabet.farshid1381@gmail.com>
2025-02-21 01:01:23 +0330

    seeding data

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import {
   ActivityIndicator,
+  Button,
   FlatList,
   Image,
   Text,
@@ -20,6 +21,7 @@ import { Card, FeaturedCard } from "@/components/Cards";
 import { useAppwrite } from "@/lib/useAppwrite";
 import { useGlobalContext } from "@/lib/global-provider";
 import images from "@/constants/images";
+import seed from "@/lib/seed";
 
 const Home = () => {
   const { user } = useGlobalContext();
@@ -30,6 +32,7 @@ const Home = () => {
 
   return (
     <SafeAreaView className="h-full bg-white">
+      <Button title="seed" onPress={seed} />
       <FlatList
         data={[1, 2, 3, 4]}
         numColumns={2}
